08c419769f85d897f2ca1d81e509cc0a4f737dcc
Maneesha Jujjuru <maneesha_jujjuru@epam.com>
2021-02-18 11:25:11 +0530

    Add project files.

---
+++ DemoActionFilters/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace DemoActionFilters.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [OutputCache(Duration = 40, VaryByParam = "none", Location = OutputCacheLocation.Client, NoStore = true)]
+        public string Index()
+        {
+            return DateTime.Now.ToString("T");
+
+        }
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            throw new Exception("Something went wrong");
+
+            //return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
